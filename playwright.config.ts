a3077e254e2efee5b8b1528d5641cb36c80cda6f
techybolek <techy_bolek@yahoo.com>
2025-05-17 19:48:09 -0500

    isolate e2e testcase

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,8 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './e2e',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
+  //forbidOnly: !!process.env.CI,
+  forbidOnly: false,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [['html', { open: 'never' }]],
